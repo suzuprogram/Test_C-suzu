34eb06845c76aac66e663319c42d7fff73c0774d
minomamusi <45559264+suzuprogram@users.noreply.github.com>
2021-05-15 15:12:42 +0900

    Test1

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public int Tasu(int a,int b)
+        {
+            int c = a + b;
+            return c;
+        }
+        
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            Class1 cls1 = new Class1();
+           int d =  cls1.Tasu(a,b);
+            Console.WriteLine(d);
+        }
+    }
+}
